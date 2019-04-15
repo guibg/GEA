916d017381f1203ef30b8feb53c81c56ec29d744
100931720 <100931720@PT1L205.ucsal.br>
2019-04-15 07:42:28 -0300

    Commit inicial

---
+++ GEA/src/main/java/com/GEA/GEA/GeaApplication.java
@@ -0,0 +1,13 @@
+package com.GEA.GEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeaApplication.class, args);
+	}
+
+}
